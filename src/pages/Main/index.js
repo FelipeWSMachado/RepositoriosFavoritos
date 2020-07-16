b00c4798f7641ec9f5d5537c493612f5851e8106
Felipe Machado <phelipecwb@gmail.com>
2020-07-16 00:04:23 -0300

    Implementando localstorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
@@ -9,15 +9,40 @@ export default function Main(){
   const [newRepo, setNewRepo] = useState('');
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState (null);
 
+  //busca no local storage
+  useEffect(()=> {
+    const repoStorage = localStorage.getItem('repos')
+    
+    if (repoStorage){
+      setRepositorios(JSON.parse(repoStorage))
+    }
+  }, [])
+   
+//salva no local storage
+useEffect(() =>{
+  localStorage.setItem('repos', JSON.stringify(repositorios))
+
+}, [repositorios])
 
   const handleSubmit = useCallback((e)=>{
     e.preventDefault();
 
     async function submit(){
       setLoading(true);
+      setAlert(null)
       try{
+        if (newRepo === ''){
+           throw new Error ('Você precisa informar um repositorio')
+        }
         const response = await api.get(`repos/${newRepo}`);
+
+        const hasRepo = repositorios.find(repo => repo.name === newRepo)
+
+        if(hasRepo){
+          throw new Error ('Repositorio Duplicado')
+        }
   
         const data = {
           name: response.data.full_name,
@@ -26,7 +51,9 @@ export default function Main(){
         setRepositorios([...repositorios, data]);
         setNewRepo('');
       }catch(error){
+        setAlert(true)
         console.log(error);
+
       }finally{
         setLoading(false);
       }
@@ -39,6 +66,7 @@ export default function Main(){
 
   function handleinputChange(e){
     setNewRepo(e.target.value);
+    setAlert(null)
   }
 
   const handleDelete = useCallback((repo) =>{
@@ -55,7 +83,7 @@ export default function Main(){
         Meus Repositorios
       </h1>
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error ={alert}>
         <input 
         type="text" 
         placeholder="Adicionar Repositorios"
